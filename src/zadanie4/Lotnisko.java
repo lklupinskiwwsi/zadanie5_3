4da02c7af6d8216cfa55eb93b0c8448e32a68d90
lesze <lesze@192.168.0.24>
2019-02-12 20:47:18 +0100

    pierwszy commit

---
+++ src/zadanie4/Lotnisko.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie4;
+
+import java.awt.geom.Point2D;
+import static java.lang.Thread.sleep;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author lesze
+ */
+public class Lotnisko {
+    private String kod;
+    private Point2D.Double polozenie;
+    private Lock l1, l2;
+   
+    public Lotnisko(String kod, Point2D.Double polozenie) {
+        this.kod = kod;
+        this.polozenie = polozenie;
+        this.l1 = new ReentrantLock();
+        this.l2 = new ReentrantLock();
+    }
+    
+    public String getKod()
+    {
+        return kod;
+    }
+    
+    public Point2D.Double getPolozenie()
+    {
+        return polozenie;
+    }
+    
+    public void Start(Samolot s) throws InterruptedException {
+        s.setStatus("Samolot " + s.getKod() + " oczekuje na start z " + this.kod);
+        sleep(2000);
+        l1.lock();
+        try {
+            s.setStatus("Samolot " + s.getKod() + " startuje z " + this.kod);
+            sleep(2000);
+        } finally {
+            l1.unlock();
+        }
+    }
+    
+    public void Ladowanie(Samolot s) throws InterruptedException {
+        s.setStatus("Samolot " + s.getKod() + " oczekuje na lądowanie na " + this.kod);
+        sleep(2000);
+        l1.lock();
+        try {
+            s.setStatus("Samolot " + s.getKod() + " ląduje na " + this.kod);
+            sleep(2000);
+        } finally {
+            l1.unlock();
+        }
+    }
+    
+    public void Tankowanie(Samolot s) throws InterruptedException {
+        s.setStatus("Samolot " + s.getKod() + " oczekuje na tankowanie na " + this.kod);
+        sleep(2000);
+        l2.lock();
+        try {
+            s.setStatus("Samolot " + s.getKod() + " tankuje na " + this.kod);
+            sleep(2000);
+        } finally {
+            l2.unlock();
+        }
+    }
+}
---
+++ src/zadanie4/Samolot.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie4;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lesze
+ */
+public class Samolot extends Thread {
+    private int kod;
+    private String status;
+    private double odleglosc, predkosc;
+    private Lotnisko l1, l2;
+    
+    public Samolot(int kod, Lotnisko l1, Lotnisko l2){
+        this.kod = kod;
+        this.l1 = l1;
+        this.l2 = l2;
+        odleglosc = l1.getPolozenie().distance(l2.getPolozenie());
+        predkosc = 10;
+    }
+    
+    public int getKod() {
+        return kod;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+    public void run() {
+        while(true) {
+            //start
+            try { 
+                l1.Start(this);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Samolot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            //lot
+            while(odleglosc > 0) {
+                try {
+                    setStatus("Samolot "+ kod + " leci z " + l1.getKod() + " do " + l2.getKod());
+                    sleep(1000);
+                    odleglosc = odleglosc - predkosc;
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Samolot.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            odleglosc = l1.getPolozenie().distance(l2.getPolozenie());
+
+            //ladowanie
+            try { 
+                l2.Ladowanie(this);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Samolot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            //tankowanie
+            try { 
+                l2.Tankowanie(this);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Samolot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            //start
+            try { 
+                l2.Start(this);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Samolot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            //lot
+            while(odleglosc > 0) {
+                try {
+                    setStatus("Samolot "+ kod + " leci z " + l2.getKod() + " do " + l1.getKod());
+                    sleep(1000);
+                    odleglosc = odleglosc - predkosc;
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Samolot.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            odleglosc = l1.getPolozenie().distance(l2.getPolozenie());
+
+            //ladowanie
+            try { 
+                l1.Ladowanie(this);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Samolot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            //tankowanie
+            try { 
+                l1.Tankowanie(this);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Samolot.class.getName()).log(Level.SEVERE, null, ex);
+            }            
+        }
+
+    }
+   
+}
+
